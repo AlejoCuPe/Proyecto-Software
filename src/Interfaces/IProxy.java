173bfb2fd8f036617efe8e22eff7034e02ce950e
andresotro <32578368+andresotro@users.noreply.github.com>
2018-11-07 12:05:16 -0500

    Avances

--- src/Interfaces/IProxy.java
+++ src/Interfaces/IProxy.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Clases;
+package Interfaces;
 
 /**
  *
  * @author Andrés
  */
-public class Facade {
-    
+public interface IProxy {
+    public long iniciarSesion(String correo, String contraseña);
 }
